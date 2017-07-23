8852d30c6d9800d1e7a64b66b4e31c3fdb236134
Kashyap Barua <xodiac642@gmail.com>
2017-07-23 20:19:07 +0530

    Using Histograms
    Using Histograms to explore continuous variables to identify potential
    outliers or unexpected data structures.

--- Project.R
+++ Project.R
@@ -10,3 +10,12 @@ CrossTable(loan_data$loan_status)
 # Call CrossTable() on grade and loan_status
 CrossTable(loan_data$grade, loan_data$loan_status, prop.r = TRUE, prop.c = FALSE,
            prop.t = FALSE, prop.chisq = FALSE)
+# Create histogram of loan_amnt: hist_1
+hist_1 <- hist(loan_data$loan_amnt)
+
+# Print locations of the breaks in hist_1
+hist_1$breaks
+
+# Change number of breaks and add labels: hist_2
+hist_2 <- hist(loan_data$loan_amnt, breaks = 200, xlab = "Loan amount", 
+               main = "Histogram of the loan amount")
