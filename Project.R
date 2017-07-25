5ce845c13470b7a57831993440082d1cf151d0e9
Kashyap Barua <xodiac642@gmail.com>
2017-07-26 01:04:00 +0530

    To know which tree performs best in terms of accuracy.
    ptree_undersample  # 7 splits
    ptree_prior  # 9 splits
    ptree_loss_matrix  # 24 splits
    ptree_weights  # 6 splits

--- Project.R
+++ Project.R
@@ -281,3 +281,23 @@ ptree_weights <- prune(tree_weights, tree_min)
 
 # Plot the pruned tree using the rpart.plot()-package
 prp(ptree_weights, extra = 1)
+
+##Confusion matrices and accuracy of our final trees
+
+# Make predictions for each of the pruned trees using the test set.
+pred_undersample <- predict(ptree_undersample, newdata = test_set,  type = "class")
+pred_prior <- predict(ptree_prior, newdata = test_set, type = "class")
+pred_loss_matrix <- predict(ptree_loss_matrix, newdata = test_set, type = "class")
+pred_weights <- predict(ptree_weights, newdata = test_set, type = "class")
+
+# construct confusion matrices using the predictions.
+confmat_undersample <- table(test_set$loan_status, pred_undersample)
+confmat_prior <- table(test_set$loan_status, pred_prior)
+confmat_loss_matrix <- table(test_set$loan_status, pred_loss_matrix)
+confmat_weights <- table(test_set$loan_status, pred_weights)
+
+# Compute the accuracies
+acc_undersample <- sum(diag(confmat_undersample)) / nrow(test_set)
+acc_prior <- sum(diag(confmat_prior)) / nrow(test_set)
+acc_loss_matrix <- sum(diag(confmat_loss_matrix)) / nrow(test_set)
+acc_weights <- sum(diag(confmat_weights)) / nrow(test_set)
