b4718486c2a150bb8797ee2caa4a04186e8879ca
Kashyap Barua <xodiac642@gmail.com>
2017-07-26 02:26:28 +0530

    reduce the logistic regression model to only two variable without reducing the AUC
    Observed that the AUC is further increased

--- Project.R
+++ Project.R
@@ -412,3 +412,23 @@ auc(test_set$loan_status, pred_4_remove_grade)
 auc(test_set$loan_status, pred_4_remove_inc)
 auc(test_set$loan_status, pred_4_remove_emp)
 
+#Further model reduction? Check if it is possible to reduce the 
+#logistic regression model to only two variable without reducing the AUC
+
+# Build three models each time deleting one variable in log_4_remove_amnt
+log_5_remove_grade <- glm(loan_status ~ annual_inc + emp_cat, family = binomial, data = training_set)  
+log_5_remove_inc <- glm(loan_status ~ grade  + emp_cat , family = binomial, data = training_set)
+log_5_remove_emp <- glm(loan_status ~ grade + annual_inc, family = binomial, data = training_set)
+
+# Make PD-predictions for each of the models
+pred_5_remove_grade <- predict(log_5_remove_grade, newdata = test_set, type = "response")
+pred_5_remove_inc <- predict(log_5_remove_inc, newdata = test_set, type = "response")
+pred_5_remove_emp <- predict(log_5_remove_emp, newdata = test_set, type = "response")
+
+# Compute the AUCs
+auc(test_set$loan_status, pred_5_remove_grade)
+auc(test_set$loan_status, pred_5_remove_inc)
+auc(test_set$loan_status, pred_5_remove_emp)
+
+# Plot the ROC-curve for the best model here
+plot(roc(test_set$loan_status,pred_4_remove_amnt))
