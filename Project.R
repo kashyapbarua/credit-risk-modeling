21cac7854cea86b98e5241ece5826e4f6c73af58
Kashyap Barua <xodiac642@gmail.com>
2017-07-26 02:18:56 +0530

    Another Round of pruning based on AUC
    See whether the AUC can still be improved by deleting another variable
    from the model.

--- Project.R
+++ Project.R
@@ -367,3 +367,48 @@ auc(ROC_probit)
 auc(ROC_cloglog)
 auc(ROC_all_full)
 
+
+##ROC-curves for comparison of tree-based models
+
+# Construct the objects containing ROC-information
+ROC_undersample <- roc(test_set$loan_status, predictions_undersample)
+ROC_prior <- roc(test_set$loan_status, predictions_prior)
+ROC_loss_matrix <- roc(test_set$loan_status, predictions_loss_matrix)
+ROC_weights <- roc(test_set$loan_status, predictions_weights)
+
+# Draw the ROC-curves in one plot
+plot(ROC_undersample)
+lines(ROC_prior, col = "blue")
+lines(ROC_loss_matrix, col = "red")
+lines(ROC_weights, col = "green")
+
+# Compute the AUCs
+auc(ROC_undersample)
+auc(ROC_prior)
+auc(ROC_loss_matrix)
+auc(ROC_weights)
+
+##Another round of pruning based on AUC
+
+# Build four models each time deleting one variable in log_3_remove_ir
+log_4_remove_amnt <- glm(loan_status ~ grade + annual_inc + emp_cat, 
+                         family = binomial, data = training_set) 
+log_4_remove_grade <- glm(loan_status ~ loan_amnt + annual_inc + emp_cat,
+                          family = binomial, data = training_set)
+log_4_remove_inc <- glm(loan_status ~ loan_amnt + grade + emp_cat,
+                        family = binomial, data = training_set)
+log_4_remove_emp <- glm(loan_status ~ loan_amnt + grade + annual_inc,
+                        family = binomial, data = training_set)
+
+# Make PD-predictions for each of the models
+pred_4_remove_amnt <- predict(log_4_remove_amnt, newdata = test_set, type = "response")
+pred_4_remove_grade <- predict(log_4_remove_grade, newdata = test_set, type = "response")
+pred_4_remove_inc <- predict(log_4_remove_inc, newdata = test_set, type = "response")
+pred_4_remove_emp <- predict(log_4_remove_emp, newdata = test_set, type = "response")
+
+# Compute the AUCs
+auc(test_set$loan_status, pred_4_remove_amnt)
+auc(test_set$loan_status, pred_4_remove_grade)
+auc(test_set$loan_status, pred_4_remove_inc)
+auc(test_set$loan_status, pred_4_remove_emp)
+
